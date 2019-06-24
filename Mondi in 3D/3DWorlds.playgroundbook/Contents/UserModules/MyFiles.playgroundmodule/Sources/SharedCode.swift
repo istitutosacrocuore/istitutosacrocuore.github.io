f5e1a9641a3f2037e36c037dd8c1f0e7ea567f23
Domenico Caggiano <domenico.caggiano@gmail.com>
2019-06-24 12:00:40 +0200

    upload

---
+++ Mondi in 3D/3DWorlds.playgroundbook/Contents/UserModules/MyFiles.playgroundmodule/Sources/SharedCode.swift
@@ -0,0 +1,8 @@
+//  Copyright © 2016-2019 Apple Inc. All rights reserved.
+
+//#-localizable-zone(PuzzleWorld01)
+// Shared Code
+// Code written in this file is available on all pages in this Playground Book.
+//#-end-localizable-zone
+
+public let world = GridWorld(columns: 10, rows: 10)
