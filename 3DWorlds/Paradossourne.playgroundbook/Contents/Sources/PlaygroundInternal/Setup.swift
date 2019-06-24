b59a59ea97a61dad2b53e90cea8c7566e0363814
Domenico Caggiano <domenico.caggiano@gmail.com>
2019-06-24 12:51:54 +0200

    commit

--- 3DWorlds/Paradossourne.playgroundbook/Contents/Sources/PlaygroundInternal/Setup.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import PlaygroundSupport
-import GameplayKit
-
-public func _setup() {
-    let viewController = UIViewController()
-    viewController.view = Canvas.shared.backingView
-    PlaygroundPage.current.liveView = viewController
-}
